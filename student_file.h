ec7e423bf36d2574cf29bcfa97051535f3bb48c9
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2023-11-24 20:30:50 -0500

    Agregando funciones a student_file.h

--- student_file.h
+++ student_file.h
@@ -1,154 +1,35 @@
-#include <iostream>
+#include <string>
 #include <vector>
 #include <map>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <fstream>
+#include <climits>
+#include <set>
 
 using namespace std;
 
-struct Road {
-    string id;
-    string city1;
-    string city2;
-    int cost;  // costo de reconstrucción, si está dañada
-
-    // Constructor para carreteras no dañadas
-    Road(const string& _id, const string& _city1, const string& _city2)
-        : id(_id), city1(_city1), city2(_city2), cost(0) {}
-
-    // Constructor para carreteras dañadas
-    Road(const string& _id, const string& _city1, const string& _city2, int _cost)
-        : id(_id), city1(_city1), city2(_city2), cost(_cost) {}
-};
-
-class RoadNetwork {
+class UnionFind {
 private:
-    map<string, vector<Road>> cityRoads;
+    map<string, string> padre;
 
 public:
-    void addRoad(const Road& road) {
-        cityRoads[road.city1].push_back(road);
-        cityRoads[road.city2].push_back(road);
+    string buscar(const string& s) {
+        if (padre.find(s) == padre.end())
+            padre[s] = s;
+        else if (padre[s] != s)
+            padre[s] = buscar(padre[s]);
+        return padre[s];
     }
 
-    string reconstructRoads() {
-        // Lista que almacenará las carreteras reconstruidas
-        vector<Road> reconstructedRoads;
-
-        // Vector para representar conjuntos disjuntos
-        map<string, string> parent;
-
-        // Función para encontrar el representante del conjunto
-        function<string(const string&)> find = [&](const string& city) -> string {
-            if (parent[city] == "" || parent[city] == city) {
-                return city;
-            } else {
-                return parent[city] = find(parent[city]);
-            }
-        };
-
-        // Función para unir dos conjuntos
-        auto unite = [&](const string& city1, const string& city2) {
-            parent[find(city1)] = find(city2);
-        };
-
-        // Ordenar las carreteras por costo
-        vector<Road> allRoads;
-        for (const auto& pair : cityRoads) {
-            allRoads.insert(allRoads.end(), pair.second.begin(), pair.second.end());
-        }
-        sort(allRoads.begin(), allRoads.end(), [](const Road& a, const Road& b) {
-            return a.cost < b.cost || (a.cost == b.cost && a.id < b.id);
-        });
-
-        // Aplicar el algoritmo de Kruskal
-        for (const Road& road : allRoads) {
-            if (find(road.city1) != find(road.city2)) {
-                unite(road.city1, road.city2);
-                reconstructedRoads.push_back(road);
-            }
-        }
-
-        // Verificar si todas las ciudades están conectadas
-        string representative = find(allRoads.front().city1);
-        for (const auto& pair : parent) {
-            if (find(pair.first) != representative) {
-                return "IMPOSIBLE";
-            }
-        }
-
-        // Construir la cadena de salida
-        stringstream result;
-        for (const Road& road : reconstructedRoads) {
-            result << road.id << " ";
-        }
-
-        return result.str().empty() ? "" : result.str();
-    }
-};
-
-// Método solicitado
-string reconstruye(vector<string>& carreteras) {
-    RoadNetwork network;
-
-    for (const string& roadStr : carreteras) {
-        istringstream iss(roadStr);
-        string id, city1, city2;
-        int cost = 0;
-
-        iss >> id >> city1 >> city2;
-
-        // Verifica si hay un costo asociado a la reconstrucción
-        if (iss >> cost) {
-            network.addRoad(Road(id, city1, city2, cost));
-        } else {
-            network.addRoad(Road(id, city1, city2));
-        }
+    void unir(const string& a, const string& b) {
+        string raizA = buscar(a);
+        string raizB = buscar(b);
+        if (raizA != raizB)
+            padre[raizA] = raizB;
     }
 
-    return network.reconstructRoads();
-}
-
-int main() {
-    // Ejemplos proporcionados
-    vector<string> example1 = {"C1 Lima Trujillo 1", "C2 Tacna Trujillo", "C3 Tacna Arequipa"};
-    vector<string> example2 = {"T1 Tumbes Quito", "C1 Iquitos StMartin 1000"};
-    vector<string> example3 = {"N1 Bogota Bucaramanga"};
-    vector<string> example4 = {"C1 Callao Ica 1", "C2 Callao Puno 2", "S0 Puno Ica"};
-
-    // Ejecuta el primer ejemplo
-    string result1 = reconstruye(example1);
-    cout << "Resultado 1: " << result1 << endl;
-
-    // Ejecuta el segundo ejemplo
-    string result2 = reconstruye(example2);
-    cout << "Resultado 2: " << result2 << endl;
-
-    // Ejecuta el tercer ejemplo
-    string result3 = reconstruye(example3);
-    cout << "Resultado 3: " << result3 << endl;
-
-    // Ejecuta el cuarto ejemplo
-    string result4 = reconstruye(example4);
-    cout << "Resultado 4: " << result4 << endl;
-
-    // Guarda el resultado del cuarto ejemplo en "student_file.h"
-    ofstream outputFile("student_file.h");
-    if (outputFile.is_open()) {
-        outputFile << "#include <vector>\n#include <string>\nusing namespace std;\n\n";
-        outputFile << "string reconstruye(vector<string>& carreteras) {\n";
-        outputFile << "    // Tu implementación aquí\n";
-        outputFile << "    return \"" << result4 << "\";\n";
-        outputFile << "}\n";
-        outputFile.close();
-        cout << "Archivo 'student_file.h' creado exitosamente.\n";
-    } else {
-        cout << "Error al abrir el archivo 'student_file.h'.\n";
+    bool estanConectados(const string& a, const string& b) {
+        return buscar(a) == buscar(b);
     }
 
-    return 0;
-}
+
 
 
