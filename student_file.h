a736cbbe59e3124d6ab5313bfa6e666d2485e826
Evelyn Milagros Chipana Ramos <116284863+emcr30@users.noreply.github.com>
2023-11-24 20:36:58 -0500

    Actualizando student_file.h

--- student_file.h
+++ student_file.h
@@ -30,6 +30,36 @@ public:
         return buscar(a) == buscar(b);
     }
 
+    bool todosConectados(const vector<string>& ciudades) {
+        if (ciudades.empty()) return true;
+        string raiz = buscar(ciudades.front());
+        for (const auto& ciudad : ciudades) {
+            if (buscar(ciudad) != raiz) return false;
+        }
+        return true;
+    }
+};
 
+struct Carretera {
+    string id;
+    string ciudad1;
+    string ciudad2;
+    int costo;
+
+    Carretera(string i, string c1, string c2, int co = -1) : id(i), ciudad1(c1), ciudad2(c2), costo(co) {}
+};
+
+vector<string> dividir(const string& str, char delim) {
+    vector<string> tokens;
+    size_t inicio = 0;
+    size_t fin = str.find(delim);
+    while (fin != string::npos) {
+        tokens.push_back(str.substr(inicio, fin - inicio));
+        inicio = fin + 1;
+        fin = str.find(delim, inicio);
+    }
+    tokens.push_back(str.substr(inicio));
+    return tokens;
+}
 
 
